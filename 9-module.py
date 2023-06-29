70c27e55cfd506f3ab1957bb4b4f974ff6d58611
Akash <kumawatap63@gmail.com>
2023-06-29 15:52:40 +0530

    change

--- 9-module.py
+++ 9-module.py
@@ -1,18 +1,18 @@
-# import module as ms
+import module as ms
 
-#  ms.secret_key(1254152)
+# ms.secret_key(1254152)
 
 # result = ms.multiply(55,66)
 
 # print(result)
 
-# x = dir(ms)
-# print(x)
+x = dir(ms)
+print(x)
 
-# details = ms.student1
+details = ms.student1
 
-# for x,y in details.items():
-#     print(x,"=",y)
+for x,y in details.items():
+    print(x,"=",y)
 
 
 # from module import student1
