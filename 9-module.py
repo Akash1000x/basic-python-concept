f8bc00bffd1a0475a783090e1f74d624afbed2c4
Akash <kumawatap63@gmail.com>
2023-06-29 15:51:09 +0530

    module concept add in 9

---
+++ 9-module.py
@@ -0,0 +1,20 @@
+# import module as ms
+
+#  ms.secret_key(1254152)
+
+# result = ms.multiply(55,66)
+
+# print(result)
+
+# x = dir(ms)
+# print(x)
+
+# details = ms.student1
+
+# for x,y in details.items():
+#     print(x,"=",y)
+
+
+# from module import student1
+
+# print(student1["name"])
---
+++ module.py
@@ -0,0 +1,17 @@
+def secret_key(password):
+    print("password is :",password)
+
+def multiply(a,b):
+    result = a*b
+    return result
+
+student1 = {
+    "name" : "ak",
+    "enr. no." : "1545512",
+    "sem" : "4th"
+}
+student2 = {
+    "name" : "vk",
+    "enr. no." : "1245512",
+    "sem" : "4th"
+}
