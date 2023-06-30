ab35af398120880d7707de97bc8cd7e718412f15
Akash <kumawatap63@gmail.com>
2023-06-30 19:38:31 +0530

    changing in 13

--- 13-read&write.py
+++ 13-read&write.py
@@ -1,20 +1,20 @@
-# x = open("textfile.txt","r")
+x = open("textfile.txt","r")
 
 
-# # x.write("I am write some text")
-# # print(x.readline())
-# # print(x.readline())
-# for a in x:
-#     print(a)
+# x.write("I am write some text")
+# print(x.readline())
+# print(x.readline())
+for a in x:
+    print(a)
     
-# x.close()
+x.close()
 
-# b = open("textfile.txt","a")
-# b.write("\nMy full name is Akash kumawat")
-# b.close()
+b = open("textfile.txt","a")
+b.write("\nMy full name is Akash kumawat")
+b.close()
 
-# c = open("textfile.txt","r")
-# print(c.read())
+c = open("textfile.txt","r")
+print(c.read())
 
 #Creat a new file
 
