53bb6070bc534211555f5b69f0cbb03bd15ca1e2
Akash <kumawatap63@gmail.com>
2023-07-03 18:44:36 +0530

    changes

---
+++ 1.1.py
@@ -0,0 +1,17 @@
+
+i = 0
+while i<5:
+    j=0
+    while j<5:
+        print("*")
+        j +=1
+    print("\n")
+    i += 1
+
+t= True
+i=0
+while t:
+    print(i)
+    i +=2
+    if i >=10:
+        t = False
